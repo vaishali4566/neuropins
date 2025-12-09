680dcf21bfeaf0bab0e2f32224a8e05e118b9afc
vaishali4566 <vaishaliverma@globussoft.in>
2025-12-09 18:38:35 +0530

    fixed layout issue

--- app/components/layout/sidebar/Sidebar.jsx
+++ app/components/layout/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Home, Upload } from "lucide-react";
+import { Home, Upload, Settings, Bell, Pin ,MessageCircleMore  } from "lucide-react";
 import SidebarItem from "./SidebarItem";
 import { useRouter } from "next/navigation";
 
@@ -8,20 +8,31 @@ export default function Sidebar() {
   const router = useRouter();
 
   return (
-    <div className="h-screen w-16 bg-[#1e1e1f] flex flex-col items-center py-6 gap-2 fixed left-0 top-0">
+    <div className="h-screen w-16 bg-[#1e1e1f] flex flex-col items-center py-6 fixed left-0 top-0">
 
-      <SidebarItem
-        icon={<Home size={18} />}
-        label="Home"
-        onClick={() => router.push("/feed")}
-      />
+  {/* Logo */}
+  {/* <div className="mb-4">
+    <img src="/logo.png" alt="Neuropins" className="w-10 h-10" />
+  </div> */}
 
-      <SidebarItem
-        icon={<Upload size={18} />}
-        label="Upload"
-        onClick={() => router.push("/upload")}
-      />
+  {/* Top Icons */}
+  <div className="flex flex-col items-center gap-2">
+    <SidebarItem icon={<Home size={18} />} label="Home" onClick={() => router.push("/feed")} />
+    <SidebarItem icon={<Upload size={18} />} label="Upload" onClick={() => router.push("/feed/upload")} />
+    <SidebarItem icon={<Pin size={18} />} label="Pins" onClick={() => router.push("/feed/pins")} />
+    <SidebarItem icon={<MessageCircleMore size={18} />} label="Chat" onClick={() => router.push("/feed/chat")} />
+    <SidebarItem icon={<Bell size={18} />} label="Notification" onClick={() => router.push("/feed/notification")} />
+  </div>
 
-    </div>
+  {/* Spacer */}
+  <div className="flex-1"></div>
+
+  {/* Bottom Settings */}
+  <div className="flex flex-col items-center gap-2 mb-4">
+    <SidebarItem icon={<Settings size={18} />} label="Settings" onClick={() => router.push("/settings")} />
+  </div>
+
+</div>
+
   );
 }
